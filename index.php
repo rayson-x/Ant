6a13201f1fedd1658390180c1bef19cecc1e15aa
alex <739849601@qq.com>
2016-08-12 01:47:30 +0800

    添加中间件测试代码

--- index.php
+++ index.php
@@ -26,20 +26,66 @@ $config = [
 //$stream = fopen('php://temp', 'w+');
 //stream_copy_to_stream(fopen('php://input', 'r'), $stream);
 //rewind($stream);
-try{
-    $stream = fopen('php://input','r');
-    $stream = new Ant\Http\Stream($stream);
-    echo $stream->read(5)."\n";
-    $stream->seek(6,SEEK_CUR);
-    echo $stream->getContents()."\n";
-    echo $stream->tell();
-    var_dump($stream->eof());
-}catch(\InvalidArgumentException $e){
-    echo $e->getMessage();
-}catch (Exception $e){
-    echo $e->getMessage();
+//try{
+//    $stream = fopen('php://input','r');
+//    $stream = new Ant\Http\Stream($stream);
+//    echo $stream->read(5)."\n";
+//    $stream->seek(1,SEEK_CUR);
+//    echo $stream->getContents()."\n";
+//    var_dump($stream->eof());
+//}catch(\InvalidArgumentException $e){
+//    echo $e->getMessage();
+//}catch (Exception $e){
+//    echo $e->getMessage();
+//}
+$abc = function(){
+    echo "this is abc start \n";
+    yield 1;
+    echo "this is abc end \n";
+};
+
+$qwe = function (){
+    echo "this is qwe start \n";
+    yield 2;
+    echo "this is qwe end \n";
+};
+//闭包函数
+$handlers = [$abc,$qwe];
+$stack = [];
+$result = null;
+foreach ($handlers as $handler) {
+    // reset before each loop, only save last handler return value
+    $result = null;
+    $generator = call_user_func_array($handler, []);
+
+    if ($generator instanceof \Generator) {
+        $stack[] = $generator;
+
+        $yield_value = $generator->current();
+    } elseif ($generator !== null) {
+        // return without yield
+        $result = $generator;
+    }
 }
 
+$return = ($result !== null);
+while ($generator = array_pop($stack)) {
+    if ($return) {
+        $generator->send($result);
+    }
+
+    $generator->next();
+
+    // PHP7之前没有Generator::getReturn()
+    if (version_compare(PHP_VERSION, '7.0.0', '<')) {
+        $return = true;
+    }
+
+    if (!$return) {
+        $return = true;
+        $result = $generator->getReturn();
+    }
+}
 
 
 
